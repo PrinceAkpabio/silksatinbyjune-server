b501983bda9f4d53f5ec2ab438619122567eb390
prince <princeakpabio90@gmail.com>
2021-06-10 23:57:33 +0100

    Added querying lists by last item id for easy pagination of data on the client

--- util/statusManager.js
+++ util/statusManager.js
@@ -6,13 +6,21 @@
  * @param {object} data
  * @returns object
  */
-module.exports.statusManager = (res, status, msg, data = []) => {
+module.exports.statusManager = (res, status, msg, data = [], params = null) => {
   // Handles responses which return data to the client
-  if (data[0] !== undefined) {
+  if (data[0] !== undefined && params === null) {
     return res.status(status).json({
       error: status <= 201 ? false : true,
       message: msg,
       data: data,
+    });
+  } else if (params !== null) {
+    // Handles responses which return data to the client with extra parameters for continous querying of the db
+    return res.status(status).json({
+      error: status <= 201 ? false : true,
+      message: msg,
+      data: data,
+      params: params,
     });
   } else {
     // Handles confirmation/boolean responses sent to the client
